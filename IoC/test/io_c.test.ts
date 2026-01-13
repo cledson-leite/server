2af9d1d5fbaebc3953692ef0ce3f91c63fffc1d2
cledson leite <40147313+cledson-leite@users.noreply.github.com>
2026-01-13 14:23:59 -0300

    create IoC and first MS

---
+++ IoC/test/io_c.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as IoC from '../lib/io_c-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/io_c-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new IoC.IoCStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
